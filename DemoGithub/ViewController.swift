e22a18a825156cbb0f0967148a70306192156a94
Kha <khahna@galaxy.com.vn>
2022-08-08 14:53:45 +0700

    update main

--- DemoGithub/ViewController.swift
+++ DemoGithub/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //update code
+        //update code on main
     }
 
 
